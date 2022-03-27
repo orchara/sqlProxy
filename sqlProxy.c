4c714b4cbefe1c54876a618ed61cd57216f46211
Vyacheslav Makarov <orchara69@gmail.com>
2022-03-27 16:11:31 +0500

    NewReSend func, whith dynamic buf is done

--- sqlProxy.c
+++ sqlProxy.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/poll.h>
 #include <netdb.h>
 #include <string.h>
 #include <stdlib.h>
@@ -46,10 +47,10 @@ int BindPort(const char *port, int direction){
             perror("server: socket \n");
             continue;
         }
-        if(setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof yes) == -1){
-            perror("server: setsockopt error \n");
-            continue;
-        }
+        // if(setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof yes) == -1){
+        //     perror("server: setsockopt error \n");
+        //     continue;
+        // }
         if(direction == IN)
         {
             if(bind(sock_fd, servinfo->ai_addr, servinfo->ai_addrlen) == -1){
@@ -105,50 +106,46 @@ int ReSend(int sock_in_fd, int sock_out_fd, char *buf){
 }
 
 int NewReSend(int sock_in_fd, int sock_out_fd, char *d_buf){
-    
     int recv_size = 1;
     u_char header[HEADSIZE];
-    do{
-        memset(header, 0, 4);
-        recv_size = recv(sock_in_fd, header, HEADSIZE, 0);
-        printf("sock_in_fd %d \n recv_size %d \n", sock_in_fd, recv_size);
-        if(recv_size == -1){
-            close(sock_in_fd);
-            perror("recv header");
-            return -1;
-        }else if(recv_size == 0){
-            break;
-        }
+    memset(header, 0, 4);
+    recv_size = recv(sock_in_fd, header, HEADSIZE, 0);
+    if(recv_size == -1){
+        close(sock_in_fd);
+        perror("recv header");
+        return -1;
+    }else if(recv_size == 0){
+        return 0;
+    }
 
-        uint32_t pack_size;
-        pack_size = ((uint32_t)header[0] | (uint32_t)header[1] << 8 | (uint32_t)header[2] << 16);
-        
-        d_buf = malloc(pack_size + HEADSIZE);
-        memset(d_buf, 0, (pack_size + HEADSIZE));
-        memcpy(d_buf, header, HEADSIZE);
-        char *s_buf = NULL;
-        s_buf = d_buf + HEADSIZE;
-
-        recv_size = recv(sock_in_fd, s_buf, pack_size, 0);
-        if(recv_size == -1){
-            close(sock_in_fd);
-            perror("recv");
-            exit(1);
-        }
+    uint32_t pack_size;
+    pack_size = ((uint32_t)header[0] | (uint32_t)header[1] << 8 | (uint32_t)header[2] << 16);
     
+    d_buf = malloc(pack_size + HEADSIZE);
+    memset(d_buf, 0, (pack_size + HEADSIZE));
+    memcpy(d_buf, header, HEADSIZE);
+    char *s_buf = NULL;
+    s_buf = d_buf + HEADSIZE;
 
-        if(send(sock_out_fd, d_buf, pack_size + HEADSIZE, 0) == -1)
-        {
-            perror("send");
-            close(sock_out_fd);
-            exit(1);
-        }
-        for (int i =0; i < pack_size; i++){
-        printf("%c", s_buf[i]);
-        }
-        printf("\n");
-        free(d_buf);
-    }while(recv_size > 0);
+    recv_size = recv(sock_in_fd, s_buf, pack_size, 0);
+    if(recv_size == -1){
+        close(sock_in_fd);
+        perror("recv");
+        exit(1);
+    }
+
+
+    if(send(sock_out_fd, d_buf, pack_size + HEADSIZE, 0) == -1)
+    {
+        perror("send");
+        close(sock_out_fd);
+        exit(1);
+    }
+    for (int i = 1; i < pack_size; i++){
+    printf("%c", s_buf[i]);
+    }
+    printf("\n");
+    free(d_buf);
     return recv_size;
 }
 
@@ -161,49 +158,70 @@ void DebugPrint(char *buf, int data_size){
 
 int main ()
 {
-    int sock_in_fd, server_fd, client_fd, retries = 0;
+    int sock_in_fd, server_fd, client_fd;
     struct sockaddr_storage their_addr;
     char buf[MAXDATASIZE];
     char *d_buf = NULL;
     while(1){
-        while(retries < 6){
-            sock_in_fd = BindPort(PORT_IN, IN);    
-            if(sock_in_fd == -1){
-                continue;
-            }
 
-            if(listen(sock_in_fd, BACKLOG) == -1){
-                perror("server: listen \n");
-                continue;
-            }
-
-            memset(buf, 0, sizeof buf);
-            socklen_t sin_size = sizeof their_addr;
-            client_fd = accept(sock_in_fd, (struct sockaddr *)&their_addr, &sin_size);
-            if(client_fd == -1){
-                close(client_fd);
-                perror("accept \n");
-                continue;        
-            }
-
-            char s[INET6_ADDRSTRLEN];
-            inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr), s, sizeof s);
-            printf("connected from %s \n", s);
-            break;
-        }
-        retries = 0;
-        while(retries < 6){
-            server_fd = BindPort(PORT_OUT, OUT);
-            if(server_fd == -1){
-                continue;
-            }
-            break;
-        }
-        if (server_fd < 0 || client_fd <0){
-            fprintf(stderr, "connection failed \n");
+        sock_in_fd = BindPort(PORT_IN, IN);    
+        if(sock_in_fd == -1){
             continue;
         }
-        int data_size = 0;
+
+        if(listen(sock_in_fd, BACKLOG) == -1){
+            perror("server: listen \n");
+            continue;
+        }
+
+        memset(buf, 0, sizeof buf);
+        socklen_t sin_size = sizeof their_addr;
+        client_fd = accept(sock_in_fd, (struct sockaddr *)&their_addr, &sin_size);
+        if(client_fd == -1){
+            close(client_fd);
+            perror("accept \n");
+            continue;        
+        }
+
+        char s[INET6_ADDRSTRLEN];
+        inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr), s, sizeof s);
+        printf("connected from %s \n", s);
+
+       
+
+        server_fd = BindPort(PORT_OUT, OUT);
+        if(server_fd == -1){
+            continue;
+        }
+
+        int data_size = 1;
+        while(data_size != 0){
+            int timeout = -1;
+            struct pollfd fds[2];
+            memset(fds, 0, sizeof(fds));
+            fds[0].fd =  server_fd;
+            fds[0].events = POLLIN;
+            fds[0].revents = 0;
+            fds[1].fd =  client_fd;
+            fds[1].events = POLLIN;
+            fds[1].revents = 0;
+            
+            if(poll(fds, 2, timeout) < 0){
+                perror("poll() failed");
+                exit(1);
+            }
+            if(fds[0].revents == POLLIN){
+                data_size = NewReSend(server_fd, client_fd, d_buf);
+                printf("\n server - client resend \n data_size = %d \n", data_size);
+            } else if(fds[1].revents == POLLIN){
+                data_size = NewReSend(client_fd, server_fd, d_buf); 
+                printf("\n client - server resend \n data_size = %d \n", data_size);
+            } else {
+                continue;
+            }
+        }
+        
+
         //int data_size = 10;
 
         // while (data_size != 0){
@@ -219,10 +237,6 @@ int main ()
         //     DebugPrint(buf, data_size);
         // }
 
-        do {
-            data_size = NewReSend(server_fd, client_fd, d_buf);
-            data_size = NewReSend(client_fd, server_fd, d_buf);
-        }while (1);
         
         
 
